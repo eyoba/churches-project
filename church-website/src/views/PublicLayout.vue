c9479961b4fda0228c7c8bad774f8d2a91726a31
eyob <eyob@github.com>
2025-12-16 08:23:52 +0100

    new updates

--- church-website/src/views/PublicLayout.vue
+++ church-website/src/views/PublicLayout.vue
@@ -18,7 +18,7 @@
 
     <footer>
       <div class="container text-center">
-        <p>&copy; 2026 Eritrean Orthodox Tewahedo Church in Norway. All rights reserved.</p>
+        <p>&copy; 2026 Eritrean Orthodox Tewahdo Church, Diocese of Norway. All rights reserved.</p>
       </div>
     </footer>
   </div>
